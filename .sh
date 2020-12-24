cc0445ea5f4207ca8090b4b13efa06a68a44cff0
lusuzi <letter_chat@outlook.com>
2020-12-24 13:41:53 +0800

    delete README.md

--- .sh
+++ .sh
@@ -6,7 +6,7 @@ main=index.html
 cat /dev/null > $main
 echo '<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><title>目录</title></head><body><center><h1>目录</h1><ul style="display:inline-block;">' > $main
 for i in $files; do
-  if [ $i != 'index.html' ] && [ $i != '.sh' ] && [ $i != 'README.md' ] && [ $i != '.bat' ]; then
+  if [ $i != 'index.html' ] && [ $i != '.sh' ] && [ $i != '.bat' ]; then
     echo '<li style="text-align: left;"><a href="'$i'" target="_blank">'$i'</a></li></br>' >> $main
   fi
 done
