3915c9da103c2029340cb390c07adb0e27f57839
lusuzi <letter_chat@outlook.com>
2020-12-24 13:59:18 +0800

    add adapt to mobile

--- .sh
+++ .sh
@@ -4,7 +4,7 @@
 files=$(ls)
 main=index.html
 cat /dev/null > $main
-echo '<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><title>目录</title></head><body><center><h1>目录</h1><ul style="display:inline-block;">' > $main
+echo '<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>目录</title></head><body><center><h1>目录</h1><ul style="display:inline-block;">' > $main
 for i in $files; do
   if [ $i != 'index.html' ] && [ $i != '.sh' ] && [ $i != '.bat' ]; then
     echo '<li style="text-align: left;"><a href="'$i'" target="_blank">'$i'</a></li></br>' >> $main
