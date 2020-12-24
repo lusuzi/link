2650983de027de1fd05e8f86777c995a23ea1be7
lusuzi <letter_chat@outlook.com>
2020-12-24 16:05:29 +0800

    update

--- .sh
+++ .sh
@@ -4,13 +4,13 @@
 files=$(ls)
 main=index.html
 cat /dev/null > $main
-echo '<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>目录</title></head><body><center><h1>目录</h1><img src="./images/Feynman.jpeg" /><ul style="display:inline-block;">' > $main
+echo '<!DOCTYPE html><html lang="zh-CN"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>目录</title></head><body><center><h1>目录</h1><ul style="display:inline-block;">' > $main
 for i in $files; do
-  if [ $i != 'index.html' ] && [ $i != '.sh' ] && [ $i != '.bat' ]; then
+  if [ $i != 'index.html' ] && [ $i != '.sh' ] && [ $i != '.bat' ] && [ $i != 'images/' ]; then
     echo '<li style="text-align: left;"><a href="'$i'" target="_blank">'$i'</a></li></br>' >> $main
   fi
 done
-echo '</ul></center></body></html>' >> $main
+echo '</ul><div><img src="./images/Feynman.jpeg" width="60%" /></div></center></body></html>' >> $main
 echo '首页文件生成完毕'
 
 # push to github
